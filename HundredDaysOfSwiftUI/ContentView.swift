677d910bd1fa6a9d342f07b2a7164c9fba39bf20
Serhii Kopanitskyi <skopanitskyi@gmail.com>
2021-03-30 20:35:38 +0300

    Initial Commit

---
+++ HundredDaysOfSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HundredDaysOfSwiftUI
+//
+//  Created by Сергей Копаницкий on 30.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HundredDaysOfSwiftUI/HundredDaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HundredDaysOfSwiftUIApp.swift
+//  HundredDaysOfSwiftUI
+//
+//  Created by Сергей Копаницкий on 30.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct HundredDaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
