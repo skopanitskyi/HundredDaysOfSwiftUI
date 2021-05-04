420c03ca0dfbc944467ccded2a7e854ad9154b50
Serhii Kopanitskyi <skopanitskyi@gmail.com>
2021-05-04 22:05:05 +0300

    Day-19: First challenge. Finished task.

--- HundredDaysOfSwiftUI/ContentView.swift
+++ HundredDaysOfSwiftUI/ContentView.swift
@@ -7,42 +7,72 @@
 
 import SwiftUI
 
-private enum ConversionType: String, CaseIterable, Identifiable {
-    var id: String { rawValue }
-    
-    case temperature
-    case length
-    case time
-    case volume
+struct Length: Hashable, Identifiable {
+    public let id = UUID()
+    public let unit: UnitLength
+    public let name: String
 }
 
 struct ContentView: View {
     
-    @State private var conversionType: ConversionType = .time
+    @State private var fromLengthType = 0
+    @State private var toLengthType = 0
+    @State private var value = ""
+    
+    private var dataSource: [Length] {
+        return getLengthUnits()
+    }
+    
+    private func getLengthUnits() -> [Length] {
+        return [Length(unit: .meters, name: "Meters"),
+                Length(unit: .kilometers, name: "Kilometers"),
+                Length(unit: .feet, name: "Feet"),
+                Length(unit: .yards, name: "Yards"),
+                Length(unit: .miles, name: "Miles")]
+    }
+    
+    private var result: Double? {
+        guard dataSource.indices.contains(fromLengthType),
+              dataSource.indices.contains(toLengthType),
+              let value = Double(value)
+        else { return nil }
+        let fromUnit = dataSource[fromLengthType].unit
+        let toUnit = dataSource[toLengthType].unit
+        return Measurement<UnitLength>(value: value, unit: fromUnit).converted(to: toUnit).value
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Picker("Type", selection: $conversionType) {
-                        ForEach(ConversionType.allCases, id: \.id) {
-                            Text("\($0.rawValue)")
-                                .tag($0)
+                    Picker("", selection: $fromLengthType) {
+                        ForEach(0..<dataSource.count) { index in
+                            HStack {
+                                Text(dataSource[index].name)
+                                Spacer()
+                            }
                         }
                     }
                 }
-                // Measurement
+                
                 Section {
-                    switch conversionType {
-                    case .time:
-                        Text("Time")
-                    case .temperature:
-                        Text("Temperature")
-                    case .length:
-                        Text("Length")
-                    case .volume:
-                        Text("Volume")
+                    TextField("Enter value", text: $value)
+                        .keyboardType(.numberPad)
+                }
+                
+                Section {
+                    Picker("", selection: $toLengthType) {
+                        ForEach(0..<dataSource.count) { index in
+                            HStack {
+                                Text(dataSource[index].name)
+                                Spacer()
+                            }
+                        }
                     }
+                }
+                
+                Section {
+                    Text(String(format: "%.2f", result ?? 0))
                 }
             }
             .navigationBarTitle("Converter")
