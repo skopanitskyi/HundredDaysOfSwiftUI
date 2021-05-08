589123369049ed8222666959ab4669baa8f0be2f
Serhii Kopanitskyi <skopanitskyi@gmail.com>
2021-05-09 00:02:53 +0300

    Day-24: Create custom view modifier that display header text with specific color.

--- HundredDaysOfSwiftUI/ContentView.swift
+++ HundredDaysOfSwiftUI/ContentView.swift
@@ -10,12 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+            .header(text: "Swift 5", color: .blue)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension View {
+    func header(text: String, color: Color) -> some View {
+        modifier(HeaderView(text: text, color: color))
+    }
+}
+
+struct HeaderView: ViewModifier {
+    
+    let text: String
+    let color: Color
+    
+    func body(content: Content) -> some View {
+        VStack {
+            Text(text)
+                .font(.title)
+                .fontWeight(.semibold)
+                .foregroundColor(color)
+            Spacer()
+            content
+            Spacer()
+        }
     }
 }
